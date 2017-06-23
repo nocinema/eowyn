e238754fa03d3d54580ad1b2f50ab41c8e1cee41
Luis Fernando Guedes <luis.guedes@neoway.com.br>
2017-06-22 21:43:54 -0300

    Adds home component

---
+++ app/components/home/home.js
@@ -0,0 +1,12 @@
+// Vue resources
+import Vue from 'vue';
+
+// Import templates
+import template from './home.template.html';
+
+// Instance of header component
+const HomeComponent = Vue.component('home', {
+    template
+});
+
+export default HomeComponent;
