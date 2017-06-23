f696420435384caae1eaed21dfd8986cd5f30463
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2017-06-23 02:04:35 -0300

    Many modifications in main app

--- app/main.js
+++ app/main.js
@@ -6,7 +6,8 @@ import VueRouter from 'vue-router';
 import HeaderComponent from './components/header/header.js';
 import MiddleComponent from './components/middle/middle.js';
 import MovieScheduleComponent from './components/movie-schedule/movie-schedule.js';
-
+import HomeComponent from './components/home/home.js';
+import FooterComponent from './components/footer/footer.js';
 
 // Main scss with components styles imported
 import MainCSS from './css/main.scss';
@@ -16,15 +17,14 @@ Vue.use(VueRouter);
 
 // Routes
 const routes = [
-    { path: '/home', alias: '/', component: Vue.component('home') },
-    { path: '/sobre', component: Vue.component('movie-schedule') },
+    { path: '/home', alias: '/', name: 'home', component: Vue.component('home') },
+    { path: '/sobre', name: 'about', component: Vue.component('movie-schedule') },
     { path: '/cidade/:city', name: 'schedule', component: Vue.component('movie-schedule') }
 ];
 
 const router = new VueRouter({
     routes,
-    base: window.location.href,
-    mode: 'hash'
+    base: window.location.href
 });
 
 // Instance of main app with components
